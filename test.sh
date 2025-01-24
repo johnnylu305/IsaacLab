c8cd7c801666211f73896a8da39b3e576a8aafcf
dsr <a2694815@gmail.com>
2025-01-24 16:31:01 +1100

    sig

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
-./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/2025-01-15_18-07-06/model_3840000_steps.zip --trans 0 0 0  --enable_camera
+./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/camera_image_nohirech_shift/model_2304000_steps.zip --trans 0 0 0  --enable_camera
 
-./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/2025-01-15_18-07-06/model_3840000_steps.zip --trans 4 4 0  --enable_camera
+./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/camera_image_nohirech_shift/model_2304000_steps.zip --trans 4 4 0  --enable_camera
 
-./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/2025-01-15_18-07-06/model_3840000_steps.zip --trans 4 -4 0  --enable_camera
+./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/camera_image_nohirech_shift/model_2304000_steps.zip --trans 4 -4 0  --enable_camera
 
-./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/2025-01-15_18-07-06/model_3840000_steps.zip --trans -4 4 0  --enable_camera
+./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/camera_image_nohirech_shift/model_2304000_steps.zip --trans -4 4 0  --enable_camera
 
-./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/2025-01-15_18-07-06/model_3840000_steps.zip --trans -4 -4 0  --enable_camera
-
+./isaaclab.sh -p source/standalone/mad3d/sb3_inference.py --input /home/dsr/Documents/mad3d/New_Dataset20/objaverse/preprocess/test.txt --task=MAD3D-v0 --checkpoint logs/sb3/MAD3D-v0/camera_image_nohirech_shift/model_2304000_steps.zip --trans -4 -4 0  --enable_camera
